f8d54969dfb2faf00c532d9b4ac7d283e61327e6
ubirajarapelli <ubirajaracpelli@gmail.com>
2021-03-16 12:42:08 -0300

    Add port in listener on heroku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
@@ -7,4 +8,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
